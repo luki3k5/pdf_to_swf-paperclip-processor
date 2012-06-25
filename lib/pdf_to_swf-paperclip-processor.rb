8792462abc57eceb954952fd164137bb73878715
GCesnik <Greg.Cesnik@metova.com>
2012-06-25 10:23:34 -0500

    Update master

--- lib/pdf_to_swf-paperclip-processor.rb
+++ lib/pdf_to_swf-paperclip-processor.rb
@@ -14,19 +14,19 @@ module Paperclip
     end
 
     def make
-      @logger = Logger.new(STDOUT)
       src = @file
-      dst = Tempfile.new([@basename, @format ? "%.#{@format}" : ''])
+      dst = Tempfile.new([@basename, "%.swf"])
       begin
         parameters = []
         parameters << @params
+        parameters << ":dest "
         parameters << ":source"
-        parameters << ":dest"
-        
+
         parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
-        success = Paperclip.run("pdf2swf", parameters, :source => "#{File.expand_path(dst.path)}",:dest => File.expand_path(src.path))
+
+        success = Paperclip.run("pdf2swf", parameters, :dest => "#{File.expand_path(dst.path)}", :source => File.expand_path(src.path))
       rescue Cocaine::CommandLineError => e
-        raise PaperclipError, "There was an error converting #{@basename} to swf" + e.inspect
+        raise PaperclipError, "There was an error converting #{@basename} to swf"
       end
       dst
     end
