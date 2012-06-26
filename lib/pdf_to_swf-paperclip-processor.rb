aa156fd28cbf466e1f32b3d0d16d185b80a5ca11
GCesnik <Greg.Cesnik@metova.com>
2012-06-26 11:20:46 -0500

    Update master

--- lib/pdf_to_swf-paperclip-processor.rb
+++ lib/pdf_to_swf-paperclip-processor.rb
@@ -1,9 +1,10 @@
 require "paperclip"
+require "pathname"
 module Paperclip
-    class PdfToSwf < Processor
-    
+  class PdfToSwf < Processor
+
     attr_accessor :file, :params, :format
-    
+
     def initialize file, options = {}, attachment = nil
       super
       @file           = file
@@ -15,21 +16,25 @@ module Paperclip
 
     def make
       src = @file
-      dst = Tempfile.new([@basename, "%.swf"])
+      temp_dst = Tempfile.new([@basename, "%.swf"])
+      @file_name = Pathname(@attachment.path).basename
+      @name = @file_name.to_s.split('.')
+
+      dst = File.new((File.expand_path(Rails.root) + "/public/system/swf_pdfs/" + @name[0] + "%.swf"), "w")
       begin
-        parameters = []
-        parameters << @params
-        parameters << ":dest "
-        parameters << ":source"
+      parameters = []
+      parameters << @params
+      parameters << ":dest "
+      parameters << ":source"
 
-        parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
+      parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
 
-        success = Paperclip.run("pdf2swf", parameters, :dest => "#{File.expand_path(dst.path)}", :source => File.expand_path(src.path))
-      rescue Cocaine::CommandLineError => e
-        raise PaperclipError, "There was an error converting #{@basename} to swf"
-      end
-      dst
+      Paperclip.run("pdf2swf", parameters, :dest => "#{File.expand_path(dst.path)}", :source => File.expand_path(src.path))
+    rescue Cocaine::CommandLineError => e
+      raise PaperclipError, "There was an error converting #{@basename} to swf"
     end
-    
+    dst
   end
+
+end
 end
